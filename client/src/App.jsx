8032ac1b78de982b56a78f085e5e12d4633a0362
mihirh19 <miheerhadvani990@gmail.com>
2024-05-10 23:37:43 +0530

    Remove unused files and dependencies

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ConnectWallet } from '@thirdweb-dev/react'
+const App = () => {
+   return (
+      <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+         <div className="sm:flex hidden mr-10 relative">
+            Slidebar
+         </div>
+
+      </div>
+   )
+}
+
+export default App
