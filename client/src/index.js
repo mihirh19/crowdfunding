3495de3fd909ae447c506e6b62e89abafc54961f
mihirh19 <miheerhadvani990@gmail.com>
2024-05-11 16:05:53 +0530

    Refactor App component and add routing

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import App from './App'
-
+import { StateContextProvider } from './context';
+import "./index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-   <ThirdwebProvider chainId={11155111}
+   <ThirdwebProvider activeChain={11155111}
       clientId={process.env.REACT_APP_TEMPLATE_CLIENT_ID}
    >
       <BrowserRouter>
-         <App />
+         <StateContextProvider>
+            <App />
+         </StateContextProvider>
       </BrowserRouter>
    </ThirdwebProvider>
 )
